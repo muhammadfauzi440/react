db89d718ed7a340799be63789dfa2299cd99d822
muhammadfauzi440 <muhammadfauzi9953@gmail.com>
2026-03-31 14:14:03 +0700

    buat komponen pertama, nama dan alat stardew valley

---
+++ src/components/Petani.jsx
@@ -0,0 +1,12 @@
+export default function Ladang({nama, alat}) {
+    return (
+        <div className="bg-white p-6 rounded-2xl shadow-lg border-2 border-green-200 hover:scale-105 transition-transform duration-300">
+            <h2 className="text-2xl font-bold text-green-700 mb-2">
+                {nama}
+            </h2>
+            <p className="text-gray-600 font-medium">
+                Alat Favorit: <span className="text-orange-600 font-bold">{alat}</span>
+            </p>
+        </div>
+    );
+}
