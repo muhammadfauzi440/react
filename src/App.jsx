89c552f814a5221ea7eacbc229f317c4efbbb392
muhammadfauzi440 <muhammadfauzi9953@gmail.com>
2026-04-02 10:39:21 +0700

    perbarui struktur routing dan navigasi di App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,4 @@
-import { BrowserRouter as Router, Routes, Route, Link } from 'react-router-dom'
+import { BrowserRouter, Router, Routes, Route, Link } from 'react-router-dom'
 
 import { useState } from 'react'
 
@@ -9,21 +9,25 @@ import './index.css'
 
 function App() {
   return (
-    <Router>
+    <BrowserRouter>
     <div className="font-poppins flex flex-col items-center min-h-screen bg-[url('/images/bg-stardew-blue.jpg')] bg-cover bg-center bg-fixed drop-shadow-2xl gap-8 p-10">
-      <div className="flex gap-6">
-        <Lokasi />
-      </div>
+      <nav className="w-full bg-gray-900 text-gray-100 p-4 shadow-xl rounded-2xl flex justify-around gap-8 font-bold text-lg border-b-4 border-gray-950">
+        <Link to="/" className="hover:underline hover:scale-105 transition-all">Home</Link>
+        <Link to="/lokasi" className="hover:underline hover:scale-105 transition-all">Lokasi</Link>
+        <Link to="/penduduk" className="hover:underline hover:scale-105 transition-all">Penduduk Desa</Link>
+        <Link to="/items" className="hover:underline hover:scale-105 transition-all">Items</Link>
+      </nav>
 
-      <div className='flex gap-6'>
-        <Orang />
-      </div>
-
-      <div className='flex gap-6'>
-        <Items />
+      <div className='p-10 grow flex flex-col items-center'>
+        <Routes>
+          <Route path="/"></Route>
+          <Route path="/lokasi" element={<Lokasi />} />
+          <Route path="/penduduk" element={<Orang />} />
+          <Route path="/items" element={<Items />} />
+        </Routes>
       </div>
     </div>
-    </Router>
+    </BrowserRouter>
   );
 }
 
